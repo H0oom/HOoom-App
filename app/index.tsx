ae28339103cdeb96403dbf9b531f550a6b15e6cb
pki071120 <ddametyy@gmail.com>
2025-08-22 11:47:28 +0900

    Update redirect path

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,20 @@
+import { GlobalContext } from '@/src/context';
 import { Redirect } from 'expo-router';
+import { useContext } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 
 export default function Index() {
+  const { token, ready } = useContext(GlobalContext);
+  if (!ready) {
+    // 아직 AsyncStorage에서 토큰 불러오는 중이면 로딩 표시
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" color="black" />
+      </View>
+    );
+  }
+  if (token) {
+    return <Redirect href="/users" />;
+  }
   return <Redirect href="/auth" />;
 }
