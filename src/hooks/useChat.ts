b53bde5b116a16ca2beba8a4fa5c914f9df817c2
pki071120 <ddametyy@gmail.com>
2025-09-18 20:04:55 +0900

    Update useChat hook

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -1,59 +1,22 @@
-import { useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { io } from 'socket.io-client';
+import { GlobalContext } from '../context';
 
 export type Message = {
   id: number;
-  sender: string;
-  content: string;
-  time: string;
-  isMe: boolean;
+  user_name: string;
+  message: string;
+  created_at: string;
 };
 
-export function useChat(userName: string) {
+export function useChat(id: number) {
   const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState<Message[]>([
-    {
-      id: 1,
-      sender: userName,
-      content: '안녕하세요!',
-      time: '오전 1:42',
-      isMe: false,
-    },
-    {
-      id: 2,
-      sender: '나',
-      content: '안녕하세요! 잘 지내시나요?',
-      time: '오전 1:42',
-      isMe: true,
-    },
-    {
-      id: 3,
-      sender: userName,
-      content: '네, 잘 지내고 있어요. 오늘 날씨가 좋네요!',
-      time: '오전 1:43',
-      isMe: false,
-    },
-  ]);
-
-  const sendMessage = () => {
-    if (message.trim()) {
-      setMessages([
-        ...messages,
-        {
-          id: messages.length + 1,
-          sender: '나',
-          content: message,
-          time: '오전 1:44',
-          isMe: true,
-        },
-      ]);
-      setMessage('');
-    }
-  };
+  const [messages, setMessages] = useState<Message[]>([]);
 
   return {
     message,
     setMessage,
     messages,
-    sendMessage,
+    setMessages,
   };
 }
